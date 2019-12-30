d900d2e0e7c6014b8ba7ed62b771b1f835bed4a5
Modan Mohammed shahil M <shahil@Peerbits.com>
2019-12-30 12:41:59 +0530

    commented code removed

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
-// import authReducer from "./authReducer";
 import apiReducer from "./apiCalls";
 import employeeListReducer from "./employeeListReducer";
-// import uploadImageReducer from "./uploadImageReducer";
-// import userProfileReducer from "./userReducer";
+
 
 // Combine all reducers as root reducer
 export default combineReducers({employeeListReducer,
   apiReducer,
-}); //articles: articleReducer,
+}); 
